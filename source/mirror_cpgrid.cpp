cf34089defa906c5d2d0ee8da2a7ecaf1085e8f9
Lars Vingli Odsæter <laods@statoil.com>
2014-04-29 15:36:08 +0200

    Edit copywrigth text

--- source/mirror_cpgrid.cpp
+++ source/mirror_cpgrid.cpp
@@ -1,31 +1,5 @@
-/*
-  Copyright 2009, 2010, 2013 SINTEF ICT, Applied Mathematics.
-  Copyright 2009, 2010, 2013 Statoil ASA.
-
-  This file is part of The Open Porous Media project  (OPM).
-
-  OPM is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  OPM is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#if HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include <iostream>
-
 #include <opm/core/io/eclipse/EclipseGridParser.hpp>
-
 
 /**
  * @file mirror_cpgrid.cpp
@@ -33,7 +7,7 @@
  * 
  * The input grid is mirrored in either the x- or y-direction, resulting in a periodic grid.
  *
- * @author Lars Vingli Odsæter <laods@statoil.com>
+ * @author Lars Vingli Odsæter <lars.odsater@math.ntnu.no>
  *
  */
 
