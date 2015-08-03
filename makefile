a1db03f5ac9b1ab235f44ebe2035d01f4ba70c24
Lars Vingli Odsæter <odsater@math.ntnu.no>
2015-08-03 14:33:08 +0200

    Update printForceInfo.cpp

--- makefile
+++ makefile
@@ -7,7 +7,7 @@ upscale_steadystate_analytic:
 	g++ -O2 -o bin/upscale_steadystate_analytic source/upscale_steadystate_analytic.cpp -lopmcore
 
 printForceInfo:
-	g++ -O2 -std=gnu++0x -o bin/printForceInfo source/printForceInfo.cpp -lopmcore -lopmporsol -ldunecornerpoint -llapack -lblas
+	g++ -O2 -std=gnu++0x -o bin/printForceInfo -I/private/laods/opm/RH6/april15/install/include -L/private/laods/opm/RH6/april15/install/lib64 -I/project/res/x86_64_RH_5/share/opm/dune-2.3/dune-geometry/include -I/project/res/x86_64_RH_5/share/opm/dune/dune-common/include -I/project/res/x86_64_RH_5/share/opm/dune/dune-grid/include -L/project/res/x86_64_RH_5/share/opm/dune/dune-grid/lib -I/project/res/x86_64_RH_5/share/ert/release/nightly/upstream/master/include source/printForceInfo.cpp -lopmcore -lopmporsol -ldunecornerpoint -llapack -lblas
 
 mirror_cpgrid:
 	g++ -O2 -std=gnu++0x -o bin/mirror_cpgrid source/mirror_cpgrid.cpp -lopmcore
--- source/printForceInfo.cpp
+++ source/printForceInfo.cpp
@@ -4,92 +4,137 @@
  *   3) Pressure drop in Pa
  */
 
+#include "config.h"
+
 #include <iostream>
 #include <fstream>
 
 #include <dune/grid/CpGrid.hpp>
-#include <opm/core/io/eclipse/EclipseGridParser.hpp>
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/Deck/Deck.hpp>
 #include <opm/porsol/common/GridInterfaceEuler.hpp>
 #include <opm/porsol/common/Rock.hpp>
+#include <opm/core/utility/MonotCubicInterpolator.hpp>
 
 using namespace std;
+
+
+class RockInterface
+{
+public:
+    RockInterface() {};
+    void read(const char* file_list);
+    void calcFracFlow(vector<double> visc, int points);
+    void calcSatAtVL(double fracflow);
+    const double cpAtVL(int satnum);
+    
+private:
+    vector<Opm::MonotCubicInterpolator> cp_;
+    vector<Opm::MonotCubicInterpolator> krw_;
+    vector<Opm::MonotCubicInterpolator> kro_;
+    vector<Opm::MonotCubicInterpolator> fw_;
+    vector<Opm::MonotCubicInterpolator> fw_inv_;
+    vector<double> sat_VL_;
+    int nSatnum_;
+};
+
+void RockInterface::read(const char* file_list)
+{
+    // Open file_list
+    ifstream file_list_stream(file_list, ios::in);
+    if (file_list_stream.fail()) {
+        cerr << "Error: Filename " << file_list << " not found or not readable." << endl;
+        exit(1);
+    }
+    
+    // Read rockfile one by one
+    string rockfile;
+    nSatnum_ = 0;
+    getline(file_list_stream, rockfile);
+    while ( getline(file_list_stream, rockfile) ) {
+        Opm::MonotCubicInterpolator rock_cp(rockfile.c_str(), 1, 4);
+        cp_.push_back(rock_cp);
+        Opm::MonotCubicInterpolator rock_krw(rockfile.c_str(), 1, 2);
+        krw_.push_back(rock_krw);
+        Opm::MonotCubicInterpolator rock_kro(rockfile.c_str(), 1, 3);
+        kro_.push_back(rock_kro);
+        ++nSatnum_;
+    }
+    file_list_stream.close();
+}
+
+void RockInterface::calcFracFlow(vector<double> visc, int points = 100)
+{
+    for (int r=0; r<nSatnum_; ++r) {
+        double swir = krw_[r].getMinimumX().first;
+        double swor = krw_[r].getMaximumX().first;
+        vector<double> fracflowVec(points, 0.0);
+        vector<double> s(points, 0.0);
+        for (int i=0; i<points; ++i) {
+            s[i] = swir + i*(swor-swir)/(points-1);
+            double mobW = krw_[r].evaluate(s[i]) / visc[0];
+            double mobO = kro_[r].evaluate(s[i]) / visc[1];
+            fracflowVec[i] = mobW / (mobW + mobO);
+        }
+        Opm::MonotCubicInterpolator fracflow(s, fracflowVec);
+        fw_.push_back(fracflow);
+        if (fracflow.isStrictlyMonotone()) {
+            fw_inv_.push_back(Opm::MonotCubicInterpolator(fracflow.get_fVector(), fracflow.get_xVector()));
+        }
+        else { 
+            cerr << "Frac flow function for rock " << r << " is not invertible!" << endl; 
+        }
+    }
+}
+
+void RockInterface::calcSatAtVL(double fracflow = 0.5)
+{
+    for (int r=0; r<nSatnum_; ++r) {
+        sat_VL_.push_back(fw_inv_[r].evaluate(fracflow));
+    }
+}
+
+const double RockInterface::cpAtVL(int satnum)
+{
+    assert(satnum <= nSatnum_);
+    return cp_[satnum-1].evaluate(sat_VL_[satnum-1]);
+}
+
 
 int main(int varnum, char** vararg) {
     
     // Read input
     const char* ECLIPSEFILENAME(vararg[1]);
-    int dir = atoi(vararg[2]);
-    double sigma = atof(vararg[3]);
-    double deltap = atof(vararg[4]);
+    const int dir = atoi(vararg[2]);
+    const double sigma = atof(vararg[3]);
+    const double deltap = atof(vararg[4]);
     const string section_name = string(vararg[5]);
+    const char* ROCKLIST(vararg[6]);
+    const double viscW = atof(vararg[7]);
+    const double viscO = atof(vararg[8]);
+    const double satur_for_visc = atof(vararg[9]);
      
     // Cap pressure data rocks and map poro and satnum
     // OBS!!! HARD-CODED!
+    bool isParallel = false;
     double Pc_avg[20] = { };
     double Pb[20] = { };
-    map<double,int> map_poro_satnum;
     if (section_name == "SN74" || section_name == "sn74") {
-        auto init = initializer_list<double>({23771,86713,1987,50694,28470,66404,26280,58528,4547,79796,61050,10623});
-	copy(init.begin(), init.end(), Pc_avg);
-	init =initializer_list<double>({3038,12617,679,3712,5526,8420,4949,9626,1440,14447,8060,3386});
-	copy(init.begin(), init.end(), Pb);
-	map_poro_satnum[0.1933] = 1;
-	map_poro_satnum[0.1033] = 2;
-	map_poro_satnum[0.27] = 3;
-	map_poro_satnum[0.1755] = 4;
-	map_poro_satnum[0.2249] = 5;
-	map_poro_satnum[0.1533] = 6;
-	map_poro_satnum[0.203] = 7;
-	map_poro_satnum[0.1701] = 8;
-	map_poro_satnum[0.2734] = 9;
-	map_poro_satnum[0.0801] = 10;
-	map_poro_satnum[0.12] = 11;
-	map_poro_satnum[0.24] = 12;
+	Pc_avg = {23771,86713,1987,50694,28470,66404,26280,58528,4547,79796,61050,10623};
+	Pb = {3038,12617,679,3712,5526,8420,4949,9626,1440,14447,8060,3386};
     }
     else if (section_name == "SN82" || section_name == "sn82") {
-        auto init = initializer_list<double>({23771,86713,50694,20412,2882,31040,4547,79796,20369,10623});
-	copy(init.begin(), init.end(), Pc_avg);
-	init = initializer_list<double>({3038,12617,3712,1630,1153,10224,1440,14447,1573,3386});
-	copy(init.begin(), init.end(), Pb);
-	map_poro_satnum[0.1933] = 1;
-	map_poro_satnum[0.1033] = 2;
-	map_poro_satnum[0.1755] = 3;
-	map_poro_satnum[0.215] = 4;
-	map_poro_satnum[0.2348] = 5;
-	map_poro_satnum[0.2098] = 6;
-	map_poro_satnum[0.2734] = 7;
-	map_poro_satnum[0.0801] = 8;
-	map_poro_satnum[0.2149] = 9;
-	map_poro_satnum[0.24] = 10;
+	Pc_avg = {23771,86713,50694,20412,2882,31040,4547,79796,20369,10623};
+	Pb = {3038,12617,3712,1630,1153,10224,1440,14447,1573,3386};
     }
     else if (section_name == "SN91" || section_name == "sn91") {
-        auto init = initializer_list<double>({23771,86713,1987,50694,78336,3516,9222,66404,6282,5675,20412,26280,4547,79796,7158});
-	copy(init.begin(), init.end(), Pc_avg);
-	init = initializer_list<double>({3038,12617,679,3712,17123,1327,1869,8420,1188,1401,1630,4949,1440,14447,1775});
-	copy(init.begin(), init.end(), Pb);
-	map_poro_satnum[0.1933] = 1;
-	map_poro_satnum[0.1033] = 2;
-	map_poro_satnum[0.27] = 3;
-	map_poro_satnum[0.1755] = 4;
-	map_poro_satnum[0.15] = 5;
-	map_poro_satnum[0.2299] = 6;
-	map_poro_satnum[0.2534] = 7;
-	map_poro_satnum[0.1532] = 8;
-	map_poro_satnum[0.2428] = 9;
-	map_poro_satnum[0.2098] = 10;
-	map_poro_satnum[0.215] = 11;
-	map_poro_satnum[0.203] = 12;
-	map_poro_satnum[0.2734] = 13;
-	map_poro_satnum[0.0801] = 14;
-	map_poro_satnum[0.2605] = 15;
+	Pc_avg = {23771,86713,1987,50694,78336,3516,9222,66404,6282,5675,20412,26280,4547,79796,7158};
+	Pb = {3038,12617,679,3712,17123,1327,1869,8420,1188,1401,1630,4949,1440,14447,1775};
     }
     else if (section_name == "Parallel" || section_name == "parallel") {
-        auto init = initializer_list<double>({1.2286e4,2.7473e5});
-	copy(init.begin(), init.end(), Pc_avg);
-	init = initializer_list<double>({4554.9,1.0185e5});
-	copy(init.begin(), init.end(), Pb);
-	map_poro_satnum[0.2] = 1;
-	map_poro_satnum[0.1] = 2;
+	Pc_avg = {1.2286e4,2.7473e5};
+	Pb = {4554.9,1.0185e5};
+	isParallel = true;
     }
     else {
 	cerr << "Model type (argument 5: '" << section_name << "') unknown. Either SN74, SN82, SN91 or Parallel.";
@@ -104,99 +149,118 @@ int main(int varnum, char** vararg) {
     }
     eclipsefile.close();
     
-    Opm::EclipseGridParser * eclParser_p;
-    try {
-        eclParser_p = new Opm::EclipseGridParser(ECLIPSEFILENAME);
-    }
-    catch (...) {
-        cout << "Error: Filename " << ECLIPSEFILENAME << " does not look like an eclipse grid file." << endl;
-        exit(1);
-    }
-    Opm::EclipseGridParser& eclParser = *eclParser_p;
+    // Parse grdecl file
+    //std::cout << "Parsing grid file '" << ECLIPSEFILENAME << "' ..." << std::endl;
+    Opm::ParserPtr parser(new Opm::Parser);
+    Opm::DeckConstPtr deck(parser->parseFile(ECLIPSEFILENAME));
 
     Dune::CpGrid grid;
-    grid.processEclipseFormat(eclParser, 0, false);
+    grid.processEclipseFormat(deck, 0, false);
     
     typedef Opm::GridInterfaceEuler<Dune::CpGrid> GridInterface;
     GridInterface gridinterf(grid);
+
+    std::vector<int> satnum_from_deck = deck->getKeyword("SATNUM")->getIntData();
+    std::vector<int> satnum(grid.globalCell().size());
+    for (int i=0; i<satnum.size(); ++i) {
+      satnum[i] = satnum_from_deck[grid.globalCell()[i]];
+    }
     
     Opm::Rock<3> rock;
-    rock.init(eclParser, grid.globalCell());
+    rock.init(deck, grid.globalCell());
    
+    // Read and process rock curves
+    RockInterface ri;
+    ri.read(ROCKLIST);
+    vector<double> visc;
+    visc.push_back(viscW);
+    visc.push_back(viscO);
+    ri.calcFracFlow(visc);
+    ri.calcSatAtVL(satur_for_visc);
+    
     double accumulated_porevol = 0.0;
     double nabla_Pc_J = 0.0;
     double nabla_Pc_avg = 0.0;
     double nabla_Pb = 0.0;
+    double nabla_Pc_VL = 0.0;
     double min_coord = 1e16;
     double max_coord = -1e16;
     
     typedef GridInterface::CellIterator CI;
     typedef CI::FaceIterator FI;
+    int i = 0;
     for (CI cell_iter = gridinterf.cellbegin(); cell_iter != gridinterf.cellend(); ++cell_iter) {
         int cell_index = cell_iter->index();
         const double cell_perm = rock.permeability(cell_index)(dir,dir);
-        const double cell_poro = rock.porosity(cell_index);
+        double cell_poro = rock.porosity(cell_index);
         const double cell_porevol = cell_iter->volume()*cell_poro;
         const double cell_lambda = sqrt(cell_perm/cell_poro);
-	if (map_poro_satnum.find(cell_poro) == map_poro_satnum.end()) {
-	    cerr << "Can't map poro " << cell_poro << " to a satnum. Check map!" << endl;
-	    exit(1);
-	}
-	const int cell_satnum = (map_poro_satnum.find(cell_poro))->second;
+	const int cell_satnum = satnum[cell_index];
         const double cell_Pc_avg = Pc_avg[cell_satnum];
         const double cell_Pb = Pb[cell_satnum];
+        const double cell_Pc_VL = ri.cpAtVL(cell_satnum);
         for (FI face_iter = cell_iter->facebegin(); face_iter != cell_iter->faceend(); ++face_iter) {
             if ( ! face_iter->boundary()) {
                 int neighbour_index = face_iter->neighbourCellIndex();
                 const double neighbour_perm = rock.permeability(neighbour_index)(dir,dir);
-                const double neighbour_poro = rock.porosity(neighbour_index);
+                double neighbour_poro = rock.porosity(neighbour_index);
                 const double neighbour_porevol = face_iter->neighbourCellVolume()*neighbour_poro;
                 const double neighbour_lambda = sqrt(neighbour_perm/neighbour_poro);
-		if (map_poro_satnum.find(neighbour_poro) == map_poro_satnum.end()) {
-		    cerr << "Can't map poro " << neighbour_poro << " to a satnum. Check map!" << endl;
-		    exit(1);
-		}
-		const int neighbour_satnum = (map_poro_satnum.find(neighbour_poro))->second;
+		const int neighbour_satnum = satnum[neighbour_index];
                 const double neighbour_Pc_avg = Pc_avg[neighbour_satnum];
                 const double neighbour_Pb = Pb[neighbour_satnum];
+                const double neighbour_Pc_VL = ri.cpAtVL(neighbour_satnum);
                 const double length = (cell_iter->centroid() - face_iter->neighbourCell().centroid()).two_norm();
                 const double porevol = cell_porevol + neighbour_porevol;
                 accumulated_porevol += porevol;
                 nabla_Pc_J += abs(1/cell_lambda - 1/neighbour_lambda)*porevol/length;
                 nabla_Pc_avg += abs(cell_Pc_avg - neighbour_Pc_avg)*porevol/length;
                 nabla_Pb += abs(cell_Pb - neighbour_Pb)*porevol/length;
+                nabla_Pc_VL += abs(cell_Pc_VL - neighbour_Pc_VL)*porevol/length;
             }
             else {
                 min_coord = min(min_coord, face_iter->centroid()[dir]);
                 max_coord = max(max_coord, face_iter->centroid()[dir]);
             }
         }
+	++i;
     }
     nabla_Pc_J = sigma*nabla_Pc_J/accumulated_porevol;
     nabla_Pc_avg = nabla_Pc_avg/accumulated_porevol;
     nabla_Pb = nabla_Pb/accumulated_porevol;
+    nabla_Pc_VL = nabla_Pc_VL/accumulated_porevol;
     
     double model_length = max_coord - min_coord;
     double nabla_visc_pressure = deltap/model_length;
     
-    cout << scientific;
-    cout << endl;
-    cout << "Pressure drop:        " << deltap << endl;
-    cout << "||nabla p||           " << nabla_visc_pressure << "\t(or " << 1/model_length << "*DeltaP)" << endl;
-    cout << endl;
-    cout << "With |J|=1:" << endl;
-    cout << "||nabla Pc||          " << nabla_Pc_J << endl;
-    cout << "Ca:                   " << nabla_visc_pressure/nabla_Pc_J << "\t(or " << 1/(model_length*nabla_Pc_J) << "*DeltaP)" << endl;
-    cout << "Ca = 1 when Delta p = " << nabla_Pc_J * model_length << endl;
-    cout << endl;
-    cout << "With Pc_avg:" << endl;
-    cout << "||nabla Pc||          " << nabla_Pc_avg << endl;
-    cout << "Ca:                   " << nabla_visc_pressure/nabla_Pc_avg << "\t(or " << 1/(model_length*nabla_Pc_avg) << "*DeltaP)" << endl;
-    cout << "Ca = 1 when Delta p = " << nabla_Pc_avg * model_length << endl;
-    cout << endl;
-    cout << "With Pb:" << endl;
-    cout << "||nabla Pc||          " << nabla_Pb << endl;
-    cout << "Ca:                   " << nabla_visc_pressure/nabla_Pb << "\t(or " << 1/(model_length*nabla_Pb) << "*DeltaP)" << endl;
-    cout << "Ca = 1 when Delta p = " << nabla_Pb * model_length << endl;
-    cout << endl;
+    if (false) {
+      cout << scientific;
+      cout << endl;
+      cout << "Pressure drop:        " << deltap << endl;
+      cout << "||nabla p||           " << nabla_visc_pressure << "\t(or " << 1/model_length << "*DeltaP)" << endl;
+      cout << endl;
+      cout << "With |J|=1:" << endl;
+      cout << "||nabla Pc||          " << nabla_Pc_J << endl;
+      cout << "Ca:                   " << nabla_visc_pressure/nabla_Pc_J << "\t(or " << 1/(model_length*nabla_Pc_J) << "*DeltaP)" << endl;
+      cout << "Ca = 1 when Delta p = " << nabla_Pc_J * model_length << endl;
+      cout << endl;
+      cout << "With Pc_avg:" << endl;
+      cout << "||nabla Pc||          " << nabla_Pc_avg << endl;
+      cout << "Ca:                   " << nabla_visc_pressure/nabla_Pc_avg << "\t(or " << 1/(model_length*nabla_Pc_avg) << "*DeltaP)" << endl;
+      cout << "Ca = 1 when Delta p = " << nabla_Pc_avg * model_length << endl;
+      cout << endl;
+      cout << "With Pb:" << endl;
+      cout << "||nabla Pc||          " << nabla_Pb << endl;
+      cout << "Ca:                   " << nabla_visc_pressure/nabla_Pb << "\t(or " << 1/(model_length*nabla_Pb) << "*DeltaP)" << endl;
+      cout << "Ca = 1 when Delta p = " << nabla_Pb * model_length << endl;
+      cout << endl;
+      cout << "With Pc at VL:" << endl;
+      cout << "||nabla Pc||          " << nabla_Pc_VL << endl;
+      cout << "Ca:                   " << nabla_visc_pressure/nabla_Pc_VL << "\t(or " << 1/(model_length*nabla_Pc_VL) << "*DeltaP)" << endl;
+      cout << "Ca = 1 when Delta p = " << nabla_Pc_VL * model_length << endl;
+      cout << endl;
+    }
+    else {
+      cout << nabla_visc_pressure/nabla_Pc_VL << endl;
+   }
 }
